97aefbafd7949542da650c81cb9f5ef8a06e40db
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-07-12 11:33:04 -0300

    adicionando testes

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_User()
+    {
+        $user = User::factory()->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+        $this->actingAs($user);
+        $response = $this->get('/users');
+
+        $response->assertStatus(200);
+    }
+    public function test_create_user()
+    {
+        $response = $this->post('/login/create', [
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => '12322132',
+            'is_admin' => 1,
+
+
+        ]);
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function test_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function  test_create_user()
+    {
+        $this->assertTrue(true);
+    }
+}
